ebc200f7775673360188da1f02c4ec175ddb6fb5
anfaas1618 <ishad0911@gmail.com>
2020-03-29 23:24:28 +0530

    ads

---
+++ app/src/main/java/io/github/froger/instamaterial/ImageProfileActivity.java
@@ -0,0 +1,14 @@
+package io.github.froger.instamaterial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImageProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_profile);
+    }
+}
