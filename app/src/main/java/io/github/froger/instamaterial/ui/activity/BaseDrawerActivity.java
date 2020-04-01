ff357b39cbefc5f069a249c161d4f0bde9c897ab
anfaas1618 <ishad0911@gmail.com>
2020-04-01 20:23:56 +0530

    reteriving and setting it to placeholders

--- app/src/main/java/io/github/froger/instamaterial/ui/activity/BaseDrawerActivity.java
+++ app/src/main/java/io/github/froger/instamaterial/ui/activity/BaseDrawerActivity.java
@@ -1,27 +1,39 @@
 package io.github.froger.instamaterial.ui.activity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
 import androidx.drawerlayout.widget.DrawerLayout;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
 import butterknife.BindDimen;
 import butterknife.BindString;
 import io.github.froger.instamaterial.R;
+import io.github.froger.instamaterial.model.User;
 import io.github.froger.instamaterial.ui.utils.CircleTransformation;
 
 /**
  * Created by Miroslaw Stanek on 15.07.15.
  */
 public class BaseDrawerActivity extends BaseActivity {
-
+    FirebaseDatabase database=FirebaseDatabase.getInstance();
+    DatabaseReference myref=database.getReference("Users");
     @BindView(R.id.drawerLayout)
     DrawerLayout drawerLayout;
     @BindView(R.id.vNavigation)
@@ -58,22 +70,44 @@ public class BaseDrawerActivity extends BaseActivity {
     }
 
     private void setupHeader() {
+        SharedPreferences uid=getSharedPreferences("UID", Context.MODE_PRIVATE);
         View headerView = vNavigation.getHeaderView(0);
+        TextView username_global= headerView.findViewById(R.id.username_global);
+        String uidSaved=uid.getString("UID",null);
         ivMenuUserProfilePhoto = (ImageView) headerView.findViewById(R.id.ivMenuUserProfilePhoto);
+        myref.child(uidSaved).child("basicInfo").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    User user = dataSnapshot.getValue(User.class);
+                    username_global.setText(user.getUsername().trim());
+                Picasso.with(BaseDrawerActivity.this)
+                        .load(user.getAvatar())
+                        .placeholder(R.drawable.img_circle_placeholder)
+                        .resize(avatarSize, avatarSize)
+                        .centerCrop()
+                        .transform(new CircleTransformation())
+                        .into(ivMenuUserProfilePhoto);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
         headerView.findViewById(R.id.vGlobalMenuHeader).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onGlobalMenuHeaderClick(v);
+
             }
         });
 
-        Picasso.with(this)
-                .load(profilePhoto)
-                .placeholder(R.drawable.img_circle_placeholder)
-                .resize(avatarSize, avatarSize)
-                .centerCrop()
-                .transform(new CircleTransformation())
-                .into(ivMenuUserProfilePhoto);
+
     }
 
     public void onGlobalMenuHeaderClick(final View v) {
